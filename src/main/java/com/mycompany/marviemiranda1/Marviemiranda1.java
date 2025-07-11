665a8a807376514955aeb04312f819244037a68b
CL2~PC01 <CL2~PC01@CL2-PC01>
2025-07-11 12:17:58 +0800

    Sample Program

---
+++ src/main/java/com/mycompany/marviemiranda1/Marviemiranda1.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.marviemiranda1;
+
+/**
+ *
+ * @author CL2~PC01
+ */
+public class Marviemiranda1 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+        System.out.println("Marvie Bautista Miranda");
+        System.out.println("19 years Old");
+    }
+}
